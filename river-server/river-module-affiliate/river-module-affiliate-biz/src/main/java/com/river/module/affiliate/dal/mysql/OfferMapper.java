88d40ccaad921b8710d5d5904d750111719727a9
xiaoheshi <shixiaohe3112@163.com>
2026-01-13 17:25:50 +0800

    feat(affiliate): 添加 Mapper 接口

---
+++ river-server/river-module-affiliate/river-module-affiliate-biz/src/main/java/com/river/module/affiliate/dal/mysql/OfferMapper.java
@@ -0,0 +1,10 @@
+package com.river.module.affiliate.dal.mysql;
+
+import com.river.framework.mybatis.core.mapper.BaseMapperX;
+import com.river.module.affiliate.dal.dataobject.OfferDO;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface OfferMapper extends BaseMapperX<OfferDO> {
+
+}
---
+++ river-server/river-module-affiliate/river-module-affiliate-biz/src/main/java/com/river/module/affiliate/dal/mysql/PostbackSecretMapper.java
@@ -0,0 +1,10 @@
+package com.river.module.affiliate.dal.mysql;
+
+import com.river.framework.mybatis.core.mapper.BaseMapperX;
+import com.river.module.affiliate.dal.dataobject.PostbackSecretDO;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface PostbackSecretMapper extends BaseMapperX<PostbackSecretDO> {
+
+}
